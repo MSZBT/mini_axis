03aedd624ee195983e2814c692f6ebcca0673a6e
Vladislav <podorozhniak_vlad@mail.ru>
2026-02-17 23:38:44 +0300

    Fatal compilation

--- main.cpp
+++ main.cpp
@@ -2,25 +2,65 @@
 //
 //#include "axis/core" //подключение core чтобы использовать логгер
 
+
 #include <QApplication>
 #include <QVBoxLayout>
 #include <QScrollArea>
 #include <QTextEdit>
 #include <memory>
-#include <Qstring>
+#include <QString>
+#include <QPushButton>
 #include <format>
 #include <string>
 
 #include <iostream>
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/base_sink.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/sinks/daily_file_sink.h"
 #include "spdlog/async.h"
+#include "spdlog/logger.h"
 /*МЕНЯТЬ ПУТИ НА ОТНОСИТЕЛЬНЫЕ*/
 
 void qtLogSent(const std::string& log_message, int level);
 
+/*РЕАЛИЗАЦИЯ ЛОГГЕРА ДЛЯ ТЕСТИРОВАНИЯ*/
+
+class Logger {
+private:
+    std::shared_ptr<spdlog::logger> m_logger;
+
+public:
+    Logger() {
+        std::vector<spdlog::sink_ptr> sinks;
+
+        auto consoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        sinks.push_back(consoleSink);
+
+        m_logger = std::make_shared<spdlog::logger>("multi_logger", sinks.begin(), sinks.end());
+        spdlog::register_logger(m_logger);
+        m_logger->set_level(spdlog::level::info);
+        m_logger->info("Logger initialized");
+    }
+
+    std::shared_ptr<spdlog::logger> getSpdlogLogger() {
+        return m_logger;
+    }
+
+    void registerSink(std::shared_ptr<spdlog::sinks::sink> sink) {
+        if (m_logger) {
+            m_logger->sinks().push_back(sink);
+        }
+    }
+
+    void info(const std::string& msg) { m_logger->info(msg); }
+    void warn(const std::string& msg) { m_logger->warn(msg); }
+    void error(const std::string& msg) { m_logger->error(msg); }
+};
+
+
+auto logger = std::make_shared<Logger>();
 
 class AppSink : public spdlog::sinks::base_sink<std::mutex> {
 protected:
@@ -39,7 +79,9 @@ protected:
 /*класс для поля выввода логов*/
 class QLoggerSpace : public QTextEdit {
 private:
-    std::shared_ptr<Logger> logger;
+    //std::shared_ptr<Logger> logger;
+    std::shared_ptr<AppSink> personalSink;
+
 public:
     QLoggerSpace() {
         setPlainText("Отслеживание логов\n");
@@ -53,18 +95,24 @@ public:
             "}"
         );
 
-        logger = Logger::getSpdlogLogger();
+        //logger = Logger::getSpdlogLogger();
+
     }
 
-    bool openSink() {
+    void openSink() {
         try {
-            if ((logger != nullptr) && (this != nullptr)) {
-                auto appSink = std::make_shared<AppSink>();
-                auto logger = Logger::getSpdlogLogger();
+            if (logger != nullptr) {
+                personalSink = std::make_shared<AppSink>();
+                logger->registerSink(personalSink);
             }
         } catch (const std::exception& e) {
-            return 1;
+            std::cout << "Error with sink opening" << std::endl;
+            return;
         }
+    }
+
+    std::shared_ptr<AppSink> getSink() {
+        return personalSink;
     }
 
     void outLogs(const QString& add_data, std::string color) {
@@ -75,8 +123,6 @@ public:
 
 /*Объект поля логов*/
 std::shared_ptr<QLoggerSpace> log_space;
-
-
 
 void qtLogSent(const std::string& log_message, int level) {
     std::string color = "white";
@@ -112,6 +158,7 @@ void qtLogSent(const std::string& log_message, int level) {
 }
 
 
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -133,11 +180,17 @@ int main(int argc, char *argv[])
     virtual_box->addWidget(scrollArea.get());
     virtual_box->setContentsMargins(10, 5, 10, 0);
 
+    QPushButton *test = new QPushButton("Press", &window);
+
+    //QObject::connect(test, &QPushButton::clicked, [log_space]() { log_space->appendText("moew", 1);}); - отправить сообещение через логгер
+    virtual_box->addWidget(test);
 
     window.setLayout(virtual_box.get());
     window.show();
 
     log_space->openSink();
 
+    /*ТЕСТИРОВАНИЕ В ВИДЕ НАПИСАНИЯ СОБСВЕННОГО ЛОГГЕРА ЛОКАЛЬНО*/
     return app.exec();
 }
+
